59edb468e87d0a06697773eb5c838a02a7ad435a
Legolas <The-Legolas>
2026-05-22 22:55:23 +0200

    Add: Maze class which creates and animates amaze if cells, although it currently only creates the start and en positions.

--- cell.py
+++ cell.py
@@ -3,7 +3,7 @@ from graphics import Window, Point, Line
 
 
 class Cell:
-    def __init__(self, win: Window) -> None:
+    def __init__(self, win: Window | None) -> None:
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
@@ -22,20 +22,36 @@ class Cell:
         self.__y1 = y1 
         self.__y2 = y2
 
+        
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
             self.__win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self.__win.draw_line(line, "white")
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
             self.__win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self.__win.draw_line(line, "white")
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
             self.__win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self.__win.draw_line(line, "white")
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self.__win.draw_line(line)
-    
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self.__win.draw_line(line, "white")
+
     def draw_move(self, to_cell: Cell, undo=False):
+        if self.__win is None:
+            return
+        
         half_length = abs(self.__x2 - self.__x1) // 2
         x_center = half_length + self.__x1
         y_center = half_length + self.__y1
