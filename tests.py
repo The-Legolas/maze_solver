65246cb06b0bdbe70cbd09de54050292bdb9925c
Legolas <The-Legolas>
2026-05-25 10:22:02 +0200

    Add: test for __reset_cells_visited

--- tests.py
+++ tests.py
@@ -38,6 +38,18 @@ class Tests(unittest.TestCase):
             cells[num_cols - 1][num_rows - 1].has_bottom_wall,
             False,
         )
+    
+    def test_maze_reset_cells_visited(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        cells = getattr(m1, "_Maze__cells")
+        for col in cells:
+            for cell in col:
+                self.assertEqual(
+                    cell.visited,
+                    False,
+                )
 
 if __name__ == "__main__":
     unittest.main()
